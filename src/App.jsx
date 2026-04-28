05e1f238f50b45e9ac2f214e381bcce2e21218ba
Rohan Gaddam <rohangaddam.dev@gmail.com>
2026-04-28 22:58:52 +0530

    Updated App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -6,30 +6,19 @@ import SelectionSortMasterclass from "./SelectionSortMasterclass";
 import MergeSortMasterclass from "./MergeSortMasterclass";
 import QuickSortMasterclass from "./QuickSortMasterclass";
 import BinarySearchMasterclass from "./BinarySearchMasterclass";
-import TwoPointersMasterclass from "./TwoPointersMasterclass";// temporary placeholders (we’ll create real ones next)
-function Placeholder({ name }) {
-  return (
-    <div style={{ color: "white", textAlign: "center", marginTop: "100px" }}>
-      <h1>{name}</h1>
-      <p>Coming soon...</p>
-    </div>
-  );
-}
+import TwoPointersMasterclass from "./TwoPointersMasterclass";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-
       <Route path="/insertion" element={<InsertionSortMasterclass />} />
-
-
-<Route path="/bubble" element={<BubbleSortMasterclass />} />
-<Route path="/selection" element={<SelectionSortMasterclass />} />
-<Route path="/merge" element={<MergeSortMasterclass />} />
-<Route path="/quick" element={<QuickSortMasterclass />} /> 
-<Route path="/binary" element={<BinarySearchMasterclass />} />
-<Route path="/two-pointers" element={<TwoPointersMasterclass />} />
-   </Routes>
+      <Route path="/bubble" element={<BubbleSortMasterclass />} />
+      <Route path="/selection" element={<SelectionSortMasterclass />} />
+      <Route path="/merge" element={<MergeSortMasterclass />} />
+      <Route path="/quick" element={<QuickSortMasterclass />} />
+      <Route path="/binary" element={<BinarySearchMasterclass />} />
+      <Route path="/two-pointers" element={<TwoPointersMasterclass />} />
+    </Routes>
   );
 }
